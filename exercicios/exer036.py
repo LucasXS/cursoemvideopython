5f63a8e60459a0772183a0b0a183da3a7fb301e1
lucas <contato_lucassantos@outlook.com>
2021-02-17 17:55:07 -0300

    Create exer036.py
    Escreva um programa para aprovar o emprestimo bancário para a compra de uma casa. O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar. Calc. o valor prestação mensal sabendo que ela não pode exceder 30% do salário ou então o emprestimo será negado.

---
+++ exercicios/exer036.py
@@ -0,0 +1,13 @@
+valorCasa = float(input('Qual o valor da casa? '))
+salarioComprador = float(input('Qual o valor de seu salário mensal? '))
+mesesPagar = int(input('Em quantos meses você vai pagar? '))
+
+valorPrestacao = valorCasa / mesesPagar
+salarioLiberado = (salarioComprador * 0.30)
+
+if valorPrestacao < salarioLiberado:
+    print(f'\033[1:31mPARABÉNS!\033[mEmprestimo Aprovado!! O valor de R${salarioLiberado}'
+          f' é suficiente para aproação do emprestimo')
+else:
+    print(f'\033[1:33m\033[1:31mREPROVADO!\033[m Infelizmente o valor de R${salarioLiberado}'
+      f' não é suficiente para a liberação do emprestimo.')
