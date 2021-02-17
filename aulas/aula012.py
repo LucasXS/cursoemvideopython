e00888f2db111e75ac0fd9bd1e6b6c7cd1d5f8fd
lucas <contato_lucassantos@outlook.com>
2021-02-17 18:02:29 -0300

    Create aula012.py
    resumo da aula 012 do Prof. Guanabara

---
+++ aulas/aula012.py
@@ -0,0 +1,11 @@
+#estrutura condiciona aninhada
+nome = str(input('Qual é seu nome: '))
+if nome == 'Lucas':
+    print('Que nome bonito')
+elif nome == 'Pedro' or nome == 'Maria' or nome == 'Paulo':
+    print('Seu nome é bem popular no Brasil')
+elif nome in 'Ana Cláudia Jessica Juliana':
+    print('Belo nome feminino.')
+else:
+    print('Seu nome é bem normal.')
+print('Tenha um bom dia {}'.format(nome))
