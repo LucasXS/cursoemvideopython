7a1d8dc061f271028d2e743c2af0a3ed655ac75b
lucas <contato_lucassantos@outlook.com>
2021-02-17 18:42:27 -0300

    Create exer037.py
    ler um número inteiro qualquer e pedir ao usuário para escolher qual será a base de conversão:
    # - 1 para binário -2 para octal -3 para hexadecimal

---
+++ exercicios/exer037.py
@@ -0,0 +1,21 @@
+num = int(input('Digite um Número Inteiro: '))
+print('='*30)
+print('     OPÇÕES DE CONVERSÃO     ')
+print('='*30)
+print('1 - para BINÁRIO')
+print('2 - para OCTAL')
+print('3 - para HEXADECIMAL')
+print('='*30)
+baseConversao = int(input('Digite o valor escolhido de conversão da tabela: '))
+
+if baseConversao == 1:
+    binario = bin(num)
+    print(f'A representação de {num} em BINÁRIO é {binario}') #desconsiderar o '0b'
+elif baseConversao == 2:
+    octal = oct(num)
+    print(f'A representação de {num} em OCTAL é {octal}') #desconsiderar o '0o'
+elif baseConversao == 3:
+    hexadecimal = hex(num)
+    print(f'A representação de {num} em HEXADECIMAL é {hexadecimal}') #desconsiderar o '0x'
+else:
+    print('Digite um valor valido da tabela!')
