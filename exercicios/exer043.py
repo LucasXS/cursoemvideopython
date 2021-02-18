a6bbbe6f187d0d2ab844e96f294d4bd4a3455a57
lucas <contato_lucassantos@outlook.com>
2021-02-18 08:25:21 -0300

    calculando IMC
    #Desenv. lógica que leia o peso e a altura de uma pessoa, calcule seu IMC e mostre seu status de acordo
    # com a tabela abaixo:
    #Abaixo de 18.5: Abaixo do peso - #Entre 15.5 e 25: Peso ideal - #25 até 30: Sobrepeso - #30 até 40: Obesidade
    #Acima de 40: Pbesidade mórbida

---
+++ exercicios/exer043.py
@@ -0,0 +1,16 @@
+nome = str(input('Seu nome: '))
+peso = float(input('Seu peso: '))
+altura = float(input('Sua altura: '))
+
+imc = peso / (altura*altura)
+
+if imc <18.5:
+    print(f'\033[1;33m{nome}\033[m, seu IMC \033[1;31m{imc:.2f}\033[m e você está ABAIXO DO PESO!')
+elif imc <25:
+    print(f'\033[1;33m{nome}\033[m, seu IMC \033[1;31m{imc:.2f}\033[m e você está no PESO IDEAL')
+elif imc <30:
+    print(f'\033[1;33m{nome}\033[m, seu IMC \033[1;31m{imc:.2f}\033[m e você está no SOBREPESO!')
+elif imc <40:
+    print(f'\033[1;33m{nome}\033[m, seu IMC \033[1;31m{imc:.2f}\033[m e você está na OBESIDADE!')
+else:
+    print(f'\033[1;33m{nome}\033[m, seu IMC \033[1;31m{imc:.2f}\033[m e você está na OBESIDADE MORBIDA')
