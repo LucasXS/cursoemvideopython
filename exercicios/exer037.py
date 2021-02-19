4e73d260385c840cf309a74bfb3af2bca39142b1
lucas <contato_lucassantos@outlook.com>
2021-02-19 08:29:35 -0300

    correções
    ficou praticamente a mesma coisa, mas aprendi a parte de fatiamento para usar no saida do tipo de conversão

--- exercicios/exer037.py
+++ exercicios/exer037.py
@@ -10,12 +10,12 @@ baseConversao = int(input('Digite o valor escolhido de conversão da tabela: '))
 
 if baseConversao == 1:
     binario = bin(num)
-    print(f'A representação de {num} em BINÁRIO é {binario}') #desconsiderar o '0b'
+    print(f'A representação de {num} em BINÁRIO é {binario[2:]}') #desconsiderar o '0b'
 elif baseConversao == 2:
     octal = oct(num)
-    print(f'A representação de {num} em OCTAL é {octal}') #desconsiderar o '0o'
+    print(f'A representação de {num} em OCTAL é {octal[2:]}') #desconsiderar o '0o'
 elif baseConversao == 3:
     hexadecimal = hex(num)
-    print(f'A representação de {num} em HEXADECIMAL é {hexadecimal}') #desconsiderar o '0x'
+    print(f'A representação de {num} em HEXADECIMAL é {hexadecimal[2:]}') #desconsiderar o '0x'
 else:
     print('Digite um valor valido da tabela!')
---
+++ exercicios/exer045.py
@@ -0,0 +1,4 @@
+#crie um programa que faça o computador jogar JOKENPÔ com você!
+
+import random
+
