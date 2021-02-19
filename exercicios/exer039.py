d4e69d2f972d97f8edf2093fce3053918c6e4f7e
lucas <contato_lucassantos@outlook.com>
2021-02-19 08:54:23 -0300

    exercicio com melhorias feitas por mim

--- exercicios/exer039.py
+++ exercicios/exer039.py
@@ -1,23 +1,27 @@
 from datetime import date
 anoAtual = date.today().year
 
-nome = str(input('Seu nome: '))
-anoNascimento = int(input('Ano de nascimento: '))
-idade = (anoAtual - anoNascimento)
-print(f'Quem nasceu em {anoNascimento} tem {idade} anos em {anoAtual}')
+sexo = str(input('Sexo - F/M: '))
 
-if idade < 18:
-    saldo = 18 - idade
-    print(f'Ainda faltam {saldo} anos para o alistamento')
-    ano = anoAtual + saldo
-    print(f'Seu alistamento será em {ano}')
-elif idade == 18:
-    print('Você tem que se alistar IMEDIATAMENTE!r')
-elif idade > 18:
-    saldo = idade - 18
-    print(f'Você já devia ter se alistado há {saldo} anos')
-    ano = anoAtual - saldo
-    print(f'Seu alistamento foi em {ano}')
+if sexo == 'M':
+    nome = str(input('Seu nome: '))
+    anoNascimento = int(input('Ano de nascimento: '))
+    idade = (anoAtual - anoNascimento)
+    print(f'Quem nasceu em {anoNascimento} tem {idade} anos em {anoAtual}')
 
+    if idade < 18:
+        saldo = 18 - idade
+        print(f'Ainda faltam {saldo} anos para o alistamento')
+        ano = anoAtual + saldo
+        print(f'Seu alistamento será em {ano}')
+    elif idade == 18:
+        print('Você tem que se alistar IMEDIATAMENTE!r')
+    elif idade > 18:
+        saldo = idade - 18
+        print(f'Você já devia ter se alistado há {saldo} anos')
+        ano = anoAtual - saldo
+        print(f'Seu alistamento foi em {ano}')
+else:
+    print('Você não tem obrigações militares!')
 
 
