62e0930a69fa6027f3f0aed5c4134e0ac00a1287
lucas <contato_lucassantos@outlook.com>
2021-02-19 08:21:34 -0300

    correção do Guanabara

--- exercicios/exer036.py
+++ exercicios/exer036.py
@@ -1,13 +1,16 @@
-valorCasa = float(input('Qual o valor da casa? '))
-salarioComprador = float(input('Qual o valor de seu salário mensal? '))
-mesesPagar = int(input('Em quantos meses você vai pagar? '))
+valorCasa = float(input('Valor da casa? R$'))
+salarioComprador = float(input('Valor de seu salário mensal? R$'))
+anosPagar = int(input('Quantos anos de financiamento? '))
 
-valorPrestacao = valorCasa / mesesPagar
-salarioLiberado = (salarioComprador * 0.30)
+valorPrestacao = valorCasa / (anosPagar * 12)
+print('Para pagar uma casa de R${:.2f} em {} anos'.format(valorCasa, anosPagar), end='')
+print(' a prestação será de R${:.2f}'.format(valorPrestacao))
 
-if valorPrestacao <= salarioLiberado:
-    print(f'\033[1:31mPARABÉNS!\033[mEmprestimo Aprovado!! O valor de R${salarioLiberado}'
+minimo = (salarioComprador * 0.30)
+
+if valorPrestacao <= minimo:
+    print(f'\033[1:31mPARABÉNS!\033[mEmprestimo Aprovado!! O valor de R${minimo}'
           f' é suficiente para aproação do emprestimo')
 else:
-    print(f'\033[1:33m\033[1:31mREPROVADO!\033[m Infelizmente o valor de R${salarioLiberado}'
+    print(f'\033[1:33m\033[1:31mREPROVADO!\033[m Infelizmente o valor de R${minimo}'
       f' não é suficiente para a liberação do emprestimo.')
