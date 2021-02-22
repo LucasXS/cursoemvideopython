a608a27f267b31ecd71d79b5065452ad62b16463
lucas <contato_lucassantos@outlook.com>
2021-02-22 08:51:39 -0300

    Update exer043.py

--- exercicios/exer043.py
+++ exercicios/exer043.py
@@ -2,7 +2,7 @@ nome = str(input('Seu nome: '))
 peso = float(input('Seu peso: '))
 altura = float(input('Sua altura: '))
 
-imc = peso / (altura*altura)
+imc = peso / (altura**2)
 
 if imc <18.5:
     print(f'\033[1;33m{nome}\033[m, seu IMC \033[1;31m{imc:.2f}\033[m e você está ABAIXO DO PESO!')
