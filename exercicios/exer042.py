d7031dc6637b2752312d31c8204208340626703b
lucas <contato_lucassantos@outlook.com>
2021-02-22 07:57:23 -0300

    Update exer042.py
    melhoria das condições dentro dos IFs

--- exercicios/exer042.py
+++ exercicios/exer042.py
@@ -6,11 +6,11 @@ c = float(input('Terceiro lado: '))
 if (a < b + c) and (b < a + c) and (c < b + a):
     print('Os segmentos acima PODEM formar um triângulo!')
         #condicionais para definir que tipo de triângulo é
-    if (a == b) and (b == c) or (b == a) and (b == c):
+    if a == b == c:
        print('É um Triângulo Equilatero - Três lados iguais')
-    elif (a == b) or (b == c) or (c == a):
-       print('É um Triânulo Isósceles - Dois lados quaisquer iguais')
+    elif a != b != c != a:
+        print('É um Triângulo Escaleno - Três lados diferentes')
     else:
-       print('É um Triângulo Escaleno - Três lados diferentes')
+        print('É um Triânulo Isósceles - Dois lados quaisquer iguais')
 else:
     print('Os segmentos acima NÃO PODEM formar um triângulo!')
