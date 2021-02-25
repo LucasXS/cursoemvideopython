863a23791c1a4cb8a0f9f9b93322ee80e716a011
lucas <contato_lucassantos@outlook.com>
2021-02-25 17:30:03 -0300

    minhas correções- Jokenpo

--- exercicios/exer045.py
+++ exercicios/exer045.py
@@ -1,14 +1,17 @@
 #crie um programa que faça o computador jogar JOKENPÔ com você!
 import random
 from  time import sleep
+
 itens = ('Pedra', 'Papel', 'Tesoura')
 computador = random.randint(0, 2)
+
 print('=-'*10)
 print('     SUAS OPÇÕES     ')
 print('=-'*10)
-print('[1] - PEDRA')
-print('[2] - PAPEL')
-print('[3] - TESOURA')
+print('[0] - PEDRA')
+print('[1] - PAPEL')
+print('[2] - TESOURA')
+
 print('=-'*10)
 jogador = int(input('Qual é a sua jogada? '))
 print('JO')
@@ -17,26 +20,35 @@ print('KE')
 sleep(0.5)
 print('PO!!!')
 sleep(0.5)
+
 if jogador == 0:
     print('O computador jogou {}'.format(itens[computador]))
     print('O jogador jogou {}'.format(itens[jogador]))
-    if itens[computador] == itens[jogador]:
-        print('Você ganhou!')
-    else:
-        print('Você perdeu!')
+    if itens[computador] == 'Pedra':
+        print('Empate')
+    elif itens[computador] == 'Papel':
+        print('Você Perdeu!')
+    elif itens[computador] == 'Tesoura':
+        print('Você Ganhou!')
+
 elif jogador == 1:
     print('O computador jogou {}'.format(itens[computador]))
     print('O jogador jogou {}'.format(itens[jogador]))
-    if itens[computador] == itens[jogador]:
-        print('Você ganhou!')
-    else:
-        print('Você perdeu!')
+    if itens[computador] == 'Pedra':
+        print('Você Ganhou!')
+    elif itens[computador] == 'Papel':
+        print('Empate!')
+    elif itens[computador] == 'Tesoura':
+        print('Você Perdeu!')
+
 elif jogador == 2:
     print('O computador jogou {}'.format(itens[computador]))
     print('O jogador jogou {}'.format(itens[jogador]))
-    if itens[computador] == itens[jogador]:
-        print('Você ganhou!')
-    else:
-        print('Você perdeu!')
+    if itens[computador] == 'Pedra':
+        print('Você perdeu!!')
+    elif itens[computador] == 'Papel':
+        print('Você Ganhou!')
+    elif itens[computador] == 'Tesoura':
+        print('Empate!')
 else:
     print('OPÇÃO INVALIDA!')
