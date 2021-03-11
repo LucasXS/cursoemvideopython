ad1fbf7f15ec5367519e545e203db29cb229f8ce
lucas <contato_lucassantos@outlook.com>
2021-03-11 08:53:52 -0300

    minor corrections in the code - exer049
    syntax improvement
    removing a line

--- exercicios/exer049.py
+++ exercicios/exer049.py
@@ -1,7 +1,6 @@
-#refazer o exercicio 09 mostrando a tabuada de um número que o usuário escolher, só que agora utlizando um laço FOR.
+# refazer o exercicio 09 mostrando a tabuada de um número que o usuário escolher, só que agora utlizando um laço FOR.
 
 num = int(input('Digite um valor da tabuada: '))
 res = 0
 for c in range(0, 11):
-    res = num * c
-    print('{} X {} = {}'.format(num, c, res))
+    print('{} X {:2} = {}'.format(num, c, num*c))
