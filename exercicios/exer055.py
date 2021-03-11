9468955820aad8668ee3197d112630d16bbad56b
lucas <contato_lucassantos@outlook.com>
2021-03-11 09:01:50 -0300

    minor corrections in the code - exer055
    comments improvement

--- exercicios/exer055.py
+++ exercicios/exer055.py
@@ -2,9 +2,9 @@ bigger = 0
 smaller = 0
 for p in range(1, 6):
     weight = float(input(f'{p}º - Your weight: '))
-    if p == 1: #li o peso da 1º pessoas
-        bigger = weight #o maior peso, vai passar a ser o peso
-        smaller = weight #o menor peso, vai passar a ser o peso
+    if p == 1:    # li o peso da 1º pessoas
+        bigger = weight     # o maior peso, vai passar a ser o peso
+        smaller = weight    # o menor peso, vai passar a ser o peso
     else:
         if weight > bigger:
             bigger = weight
