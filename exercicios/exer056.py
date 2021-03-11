7345819ac351f4f7b6d7c5a36d6df9ba11e29ce0
lucas <contato_lucassantos@outlook.com>
2021-03-11 09:02:48 -0300

    corrections in the code - exer056
    syntax improvement
    removing unnecessary lines
    adding best comments

--- exercicios/exer056.py
+++ exercicios/exer056.py
@@ -4,24 +4,20 @@ cont = 0
 mediaIdade = 0
 for c in range(1, 5):
     print(f'=={c}ª Pessoa==')
-    name = str(input('Type your name: ')).strip()
+    name = str(input('Type your name: ')).strip().upper()
     age = int(input('Type your age: '))
     sex = str(input('Type your sex [F/M]: ')).upper().strip()
     soma += age
     mediaIdade = soma / 4
-
-    if sex == 'M':
-        if c == 1:
-            bigger = age
-            mName = name
-        else:
-            if age > bigger:
-                bigger = age
-                mName = name
-    if sex == 'F':
-        if age < 21:
-            cont += 1
+    if c == 1 and sex == 'M':
+        bigger = age
+        mName = name
+    if sex in 'M' and age > bigger:
+        bigger = age
+        mName = name
+    if sex == 'F' and age < 20:
+        cont += 1
 
 print(f'A Média de idade do grupo é de {mediaIdade} anos!')
-print(f'{mName} é o homem mais velho!')
+print(f'{mName} é o homem mais velho e tem {bigger} anos!')
 print(f'{cont} mulher(es) tem menos de 21 anos')
