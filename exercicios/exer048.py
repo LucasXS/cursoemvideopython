fe8ca5a961737b4f8dba2c3c56ac956fd5862a1a
lucas <contato_lucassantos@outlook.com>
2021-03-11 08:51:51 -0300

    minor corrections in the code - exer048
    adding a counter

--- exercicios/exer048.py
+++ exercicios/exer048.py
@@ -1,8 +1,9 @@
-#Calc. a soma entre todos os números impares que são multiplos de três e que se encontram no intervalo de 1 até 500.
+# Calc. a soma entre todos os números impares que são multiplos de três e que se encontram no intervalo de 1 até 500.
 
-soma = 0
+soma = 0 #acumulador
+cont = 0 #contador
 for c in range(1, 500, 2):
     if c % 3 == 0:
-        print(c)
-        soma += c
-print('A soma de todos os valores impares e multiplos de 3 é {}'.format(soma))
+        soma += c        # acumulo valores (somando/multiplicando)
+        cont += + 1  # um contador conta +1
+print('A soma de todos os {} valores impares e multiplos de 3 é {}'.format(cont, soma))
