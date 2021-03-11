d13376ef76a119ae5d2599901756924d99e8821d
lucas <contato_lucassantos@outlook.com>
2021-03-10 23:14:49 -0300

    Update exer056.py
    exercicio completo feito por mim.
    # ler nome, idade e sexo de 4 pessoas. No final mostre: A media de idade do grupo. Qual o nome do homem mais velho.
    # Qyantas mulheres tem menos de 21 anos

--- exercicios/exer056.py
+++ exercicios/exer056.py
@@ -1,16 +1,27 @@
-# ler nome, idade e sexo de 4 pessoas. No final mostre: A media de idade do grupo. Qual o nome do homem mais velho.
-# Qyantas mulheres tem menos de 21 anos
-
 soma = 0
+bigger = 0
+cont = 0
+mediaIdade = 0
 for c in range(1, 5):
     print(f'=={c}ª Pessoa==')
     name = str(input('Type your name: ')).strip()
     age = int(input('Type your age: '))
     sex = str(input('Type your sex [F/M]: ')).upper().strip()
     soma += age
-if sex == 'M':
+    mediaIdade = soma / 4
 
-mediaIdade = soma /4
-print('A Média de idade do grupo é de {} anos'.format(mediaIdade))
+    if sex == 'M':
+        if c == 1:
+            bigger = age
+            mName = name
+        else:
+            if age > bigger:
+                bigger = age
+                mName = name
+    if sex == 'F':
+        if age < 21:
+            cont += 1
 
-
+print(f'A Média de idade do grupo é de {mediaIdade} anos!')
+print(f'{mName} é o homem mais velho!')
+print(f'{cont} mulher(es) tem menos de 21 anos')
