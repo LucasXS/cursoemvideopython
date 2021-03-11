47e587a9212e9a3fb6430650097e65d8c94befaa
lucas <contato_lucassantos@outlook.com>
2021-03-11 08:50:31 -0300

    minor corrections in the code - exer047
    removing a line

--- exercicios/exer047.py
+++ exercicios/exer047.py
@@ -1,5 +1,4 @@
-#mostre na tela todos os números pares que estão no intervalo entre 1 e 50
-
-for c in range(2, 52, 2):
-    print(c)
+# mostre na tela todos os números pares que estão no intervalo entre 1 e 50
+for c in range(2, 51, 2):
+    print(c, end=' ')
 print('Fim')
