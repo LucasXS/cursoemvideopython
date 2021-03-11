9c3534c16c915357040f9e5ad119dcb17dfa5470
lucas <contato_lucassantos@outlook.com>
2021-03-11 09:04:03 -0300

    minor corrections in the code - exer054
    syntax corrections

--- exercicios/exer54.py
+++ exercicios/exer54.py
@@ -1,8 +1,5 @@
-#ler o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram a
-# maioridade e quantos já são maiores
-
 from datetime import date
-currentYear = date.today().year #variável geral para receber o ano atula
+currentYear = date.today().year     # variável geral para receber o ano atula
 
 cont = 0
 cont2 = 0
@@ -10,9 +7,9 @@ for c in range(1, 8):
     YearOfBirth = int(input(f'{c} Type the year of birth: '))
     age = (currentYear - YearOfBirth)
     #print(f'Who was born in {YearOfBirth}, is {age} years old in {currentYear}')
-    if age < 18:
+    if age < 21:
         cont += 1
     else:
         cont2 += 1
-print(f'Ao todo tivemos {cont2} pessoas maiores de idade!')
-print(f'E também tivemos {cont} pessoas menores de idade')
+print(f'Altogether we had {cont2} older people!')
+print(f'Altogether ww had {cont} underage people!')
