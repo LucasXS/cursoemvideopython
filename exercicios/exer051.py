92d8b26e1d6bb1f7bfa5e33614d969da5b9ce713
lucas <contato_lucassantos@outlook.com>
2021-03-11 08:55:25 -0300

    minor corrections in the code - exer051
    syntax improvement

--- exercicios/exer051.py
+++ exercicios/exer051.py
@@ -3,7 +3,7 @@ print('   10 TERMOS DE UMA PA  ')
 print('='*25)
 primTermo = int(input('PRIMEIRO TERMO: '))
 razao = int(input('RAZÃO: '))
-decimo = primTermo + 10 * razao
-for c in range(primTermo, decimo, razao):
+decimo = primTermo + (10-1) * razao
+for c in range(primTermo, decimo + razao, razao):
     print('{}'.format(c), end=' ->')
 print('FIM')
