647da5c688626a576e7fcfd9d7783d9b5acdc3c1
lucas <contato_lucassantos@outlook.com>
2021-03-11 08:56:58 -0300

    adding a comment in the code - exer052

--- exercicios/exer052.py
+++ exercicios/exer052.py
@@ -1,5 +1,5 @@
 tot = 0
-num = int(input('Número: '))
+num = int(input('Número: '))    # digitar um número inteiro - é ou não um número primo
 for c in range(1, num+1):
     if num % c == 0:
         print('\033[33m', end=' ')
