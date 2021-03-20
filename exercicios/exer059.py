261335421f4af23579c1a905c46f0c7ac1867d32
lucas <contato_lucassantos@outlook.com>
2021-03-20 15:44:19 -0300

    Calculadora personalizada simples

---
+++ exercicios/exer059.py
@@ -0,0 +1,34 @@
+# ler um dois valores e mostre um menu na tela
+# 1 - somar - 2 multiplicar - 3 maior - 4 novos números - 5 sair do programa
+# seu programa deverá realizar a operação solicitada em cada caso
+
+operador = 0
+
+num1 = float(input('Valor A: '))
+num2 = float(input('Valor B: '))
+
+while operador != 5:
+    print('=-'*30)
+    print(' [1] - Somar\n [2] - Multiplicar\n [3] - Maior\n [4] - Novos números\n [5] - Sair do progrma')
+    print('=-' * 30)
+    operador = int(input('Qual é a sua opção? '))
+    if operador == 1:
+        soma = num1 + num2
+        print(f'A soma entre {num1} e {num2} é {soma}')
+    elif operador == 2:
+        multiplicacao = num1 * num2
+        print(f'A multiplicação entre {num1} e {num2} é {multiplicacao}')
+    elif operador == 3:
+        if num1 > num2:
+            print(f'O número {num1} é maior que o número {num2}')
+        elif num2 > num1:
+            print(f'O número {num2} é maior que o número {num1}')
+        else:
+            print(f'Os números são iguais!')
+    elif operador == 4:
+        # informe o número novamente
+        num1 = float(input('Valor A: '))
+        num2 = float(input('Valor B: '))
+    else:
+        print('FIM DO PROGRAMA...')
+print('ATÉ A PRÓXIMA!!')
