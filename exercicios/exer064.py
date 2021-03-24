68be3f18ee415e7c6d557ffeffa52cb619be3cc4
lucas <contato_lucassantos@outlook.com>
2021-03-24 12:26:20 -0300

    Create exer064.py
    # 64 - Ler vários números inteiros pelo teclado. O programa só vai parar
    # quando o usuário digitar o valor 999, que é a condição de parada.
    # No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag.)

---
+++ exercicios/exer064.py
@@ -0,0 +1,7 @@
+num = cont = total = 0
+num = int(input('Digite um número: '))
+while num != 999:
+    cont += 1
+    total += num
+    num = int(input('Digite um número: '))
+print(f'Você digitou {cont} números e a soma entre eles foi {total} ')
