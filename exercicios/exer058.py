bb6c4943849c8bb7a3e54a3e56a53541e983d53b
lucas <contato_lucassantos@outlook.com>
2021-03-24 12:18:09 -0300

    Update exer058.py
    Melhorias do professor Guanabara

--- exercicios/exer058.py
+++ exercicios/exer058.py
@@ -6,20 +6,27 @@ from random import randint
 from time import sleep
 
 computador = randint(0, 10)     # o computador vai pensar em número entre 0 e 10
-c = 0
-jogador = ''
+palpites = 0
+acertou = False
+
 print('START THE GAME! - VAMOS TESTAR SUA SORTE')
 print('='*60)
 print('Vou pensar em um número entre 0 e 10. Tente adivinhar...')
 print('='*60)
 
-while jogador != computador:    # condição de parada
-    jogador = int(input('Digite sua jogada: '))
-    c += 1
+while not acertou:    # condição de parada
+    jogador = int(input('Qual é o seu palpite? '))
+    palpites += 1
     sleep(0.1)
     if jogador == computador:
         sleep(0.1)
-        print(f'\033[1:33mPARABÉNS, você ganhou!\033[m Precisou de '
-              f'\033[1:31m{c}\033[m tentativas para acertar o que eu pensei!')
+        acertou = True
+    else:
+        if jogador < computador:
+            print('Mais...Tente mais uma vez')
+        elif jogador > computador:
+            print('Menos...Tente mais uma vez')
+print(f'\033[1:33mPARABÉNS, você ganhou!\033[m Precisou de '
+      f'\033[1:31m{palpites}\033[m tentativas para acertar o que eu pensei!')
 
 
