2d25c67cb95bcb314a601f9d807290a8f602f9c3
lucas <contato_lucassantos@outlook.com>
2021-03-27 21:36:36 -0300

    Update exer069.py
    melhorias no codigo.

--- exercicios/exer069.py
+++ exercicios/exer069.py
@@ -1,22 +1,21 @@
 idadeCont = sexoMas = mulherCont = 0
-continuar = 'S'
-
-while True:
+while True:     # condição de parada
     idade = int(input('Idade: '))
-    sexo = str(input('Sexo: [M/F] ')).upper().split()
-    continuar = str(input('Quer continuar? [S/N] ')).upper().split()
-
-    if idade > 18:
+    sexo = ' '
+    sexo = str(input('Sexo: [M/F] ')).upper()[0]
+    while sexo not in 'MF':
+        sexo = str(input('Sexo: [M/F] ')).upper().strip()[0]
+    if idade >= 18:
         idadeCont += 1
-    elif sexo == 'M':
+    if sexo == 'M':
         sexoMas += 1
-    elif sexo == 'F' and idade > 20:
+    if sexo == 'F' and idade < 20:
         mulherCont += 1
-    elif continuar == 'N':
-        print(f'Total de pessoas com mais de 18 anos: {idadeCont}')
-        print(f'Ao todo temos {sexoMas} homens cadastrados')
-        print(f'E temos {mulherCont} mulheres com menos de 20 anos')
+    continuar = ' '
+    while continuar not in 'SN':
+        continuar = str(input('Quer continuar? [S/N] ')).upper().strip()[0]
+    if continuar == 'N':
         break
-print(f'2Total de pessoas com mais de 18 anos: {idadeCont}')
-print(f'2Ao todo temos {sexoMas} homens cadastrados')
-print(f'2E temos {mulherCont} mulheres com menos de 20 anos')
+print(f'Total de pessoas com mais de 18 anos: {idadeCont}')
+print(f'Ao todo temos {sexoMas} homens cadastrados')
+print(f'E temos {mulherCont} mulheres com menis de 20 anos')
